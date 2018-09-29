e7359205906764c13f8be7f10ba6fb41004ea654
fitra <dwi.prasetya380@gmail.com>
2018-09-29 00:16:14 -0700

    update biodata finish

--- index.php
+++ index.php
@@ -6,30 +6,75 @@
 		<div class="content-table">
 			<table width="60%" style="font-size:14px; border-radius: 8px; border:1px solid #64abff">
 	        <tr>
-	            <td rowspan="7"><img src="img/fitra.jpg" class="img-foto-profil" alt="" style="width: 180px; height: 220px; margin-left:7%"/></td>
+	            <td rowspan="7"><img src="img/foto-copy.png" class="img-foto-profil" alt="" style="width: 180px; height: 220px; margin-left:7%"/></td>
 	        </tr>    
 	          <tr>
-	            <td>Nama&emsp;&emsp;&emsp;&emsp;:</td>
-	            <td>Fitra Dwi Prasetya</td>  
+	            <td>Nama&emsp;&emsp;&emsp;&emsp;&emsp;:</td>
+	            <td>Bagus Eko Prasetyo</td>  
+	                      
 	          </tr>
 	          <tr>
-	            <td>NPM&emsp;&emsp;&emsp;&emsp; :</td>
-	            <td>06.2016.1.06636</td>          
+	            <td>NPM&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;:</td>
+	            <td>06.2016.1.06628</td>          
 	          </tr>
 	          <tr>
-	            <td>Jurusan&emsp;&emsp;&emsp;&nbsp;:</td>
+	            <td>Jurusan&emsp;&emsp;&emsp;&emsp;&nbsp;:</td>
 	            <td>Teknik Informatika</td>          
 	          </tr>
 	          <tr>
-	            <td>Tanggal Lahir&nbsp;&nbsp; :</td>
-	            <td>07 - Februari - 1998</td>          
+	            <td>Tanggal Lahir&emsp;&nbsp;&nbsp; :</td>
+	            <td>23 - Agustus - 1997</td>          
 	          </tr>
 	          <tr>
-	           <td><button>Info Lebih Lanjut</button></td>  
-	           <td><button><a href="index2.php">Biodata Lainnya</a></button></td>          
+	           <td><button><a href="#popup" class="popup-link">Info Lebih Lanjut</a></button></td>  
+	           <td><button><a href ="index2.php">Biodata Lainnya</a></button></td>          
 	          </tr>
 	         
 	      	</table>		
+		</div>
+		<div class="popup-wrapper" id="popup">
+			<div class="popup-container">
+				<table width="100%" style="font-size:14px; border-radius: 8px; border:1px solid #64abff">
+			        <tr>
+			            <td rowspan="10"><img src="img/foto-copy.png" class="img-foto-profil" alt="" style="width: 180px; height: 220px; margin-left:7%"/></td>
+			        </tr>    
+			          <tr>
+			            <td class="font-popup">Nama&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</td>
+			            <td class="font-popup">Bagus Eko Prasetyo</td>  
+			                      
+			          </tr>
+			          <tr>
+			            <td class="font-popup">NPM&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;:</td>
+			            <td class="font-popup">06.2016.1.06628</td>          
+			          </tr>
+			          <tr>
+			            <td class="font-popup">Jurusan&emsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</td>
+			            <td class="font-popup">Informatika</td>          
+			          </tr>
+			          <tr>
+			            <td class="font-popup">Tanggal Lahir&emsp; :</td>
+			            <td class="font-popup">23 - Agustus - 1997</td>          
+			          </tr> 
+			          <tr>
+			            <td class="font-popup">Jenis Kelamin&emsp;:</td>
+			            <td class="font-popup">Laki - Laki </td>          
+			          </tr>  
+			          <tr>
+			            <td class="font-popup">Alamat&emsp;&emsp;&emsp;&emsp;:</td>
+			            <td class="font-popup">JL. Mleto 1/12C</td>          
+			          </tr>  
+			          <tr>
+			            <td class="font-popup">NO. Telp&emsp;&emsp;&emsp;&nbsp;:</td>
+			            <td class="font-popup">083857465006</td>          
+			          </tr>  
+			          <tr>
+			            <td class="font-popup">Hobby&emsp;&emsp;&emsp;&emsp;&nbsp;:</td>
+			            <td class="font-popup">Nge Game</td>          
+			          </tr>  
+			           
+	      	</table>	
+				<a class="popup-close" href="#closed">X</a>
+			</div>
 		</div>
 		
 	</section>
